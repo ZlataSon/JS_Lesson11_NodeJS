f957e6c0b1740f7ebb20022f619f41ae6430b776
Zlata <filingame12@gmail.com>
2017-01-31 01:15:11 +0200

    Create Todo List

--- clients/js/lesson11.js
+++ clients/js/lesson11.js
@@ -3,13 +3,18 @@
 
 $(function () {
 
-    var data = JSON.parse(localStorage.getItem("todoData"));
-    data = data || [[],[],[],[],[],[],[],[]];
+    // var data = JSON.parse(localStorage.getItem("todoData"));
+    // data = data || [[],[],[],[],[],[],[],[]];
 
-    $.each( data, function (index, params) {
-        $.each( params, function (indexItem, item) {
-            $('#'+index).append('<li class="Bullet">' + item + '</li>');
+    $.post('/load',function (result) {
+        var data = result[0] || [[],[],[],[],[],[],[],[]];
+
+        $.each( data, function (index, params) {
+            $.each( params, function (indexItem, item) {
+                $('#'+index).append('<li class="Bullet">' + item + '</li>');
+            });
         });
+
     });
 
      $('#todoList ul').sortable({
@@ -23,33 +28,65 @@ $(function () {
 
              //var ul = ui.item.parent()[0];
              var ul = event.target;
-             var id = +(ul.id);
-             var data = JSON.parse(localStorage.getItem("todoData"));
-             data = data || [[],[],[],[],[],[],[],[]];
-             data[id] = [];
+             //var id = +(ul.id);
+             //var data = JSON.parse(localStorage.getItem("todoData"));
+             //data = data || [[],[],[],[],[],[],[],[]];
+             $.post('/load',function (result) {
+                 var ul = this;
+                 var id = +(ul.id);
+                 var data = result[0] || [[],[],[],[],[],[],[],[]];
+                 data[id] = [];
 
-             $(ul.children).filter('.Bullet').each( function (item) {
-                  data[id].push(this.innerText);
-             });
+                 $(ul.children).filter('.Bullet').each( function (item) {
+                     data[id].push(this.innerText);
+                 });
 
-             localStorage.setItem("todoData", JSON.stringify(data));
+                 //localStorage.setItem("todoData", JSON.stringify(data));
+
+                 $.ajax({
+                     url: '/save',
+                     method: 'POST',
+                     data: JSON.stringify(data),
+                     contentType: 'application/json; charset=utf-8',
+                     // success: function (result) {
+                     //    console.log(result);
+                     // }
+                     // },data,function (result) {
+                     //     console.log('load data');
+                 })
+
+             }.bind(ul));
          },
 
          receive:function(event,ui) {
             if (ui.item.parent().length == 0) return;
-             var id = +(ui.item.parent()[0].id);
-             var data = JSON.parse(localStorage.getItem("todoData"));
-             data = data || [[],[],[],[],[],[],[],[]];
-             data[id].push(ui.item[0].innerText);
 
-             var id = +(ui.sender[0].id);
-             for (var i=0; i<data[id].length; i++) {
-                 if (data[id][i]==ui.item[0].innerText) {break;}
-             }
+            //var id = +(ui.item.parent()[0].id);
+             //var data = JSON.parse(localStorage.getItem("todoData"));
+             //data = data || [[],[],[],[],[],[],[],[]];
+             $.post('/load',function (result) {
+                 var ui =this;
+                 var id = +(ui.item.parent()[0].id);
+                 var data = result[0] || [[],[],[],[],[],[],[],[]];
+                 data[id].push(ui.item[0].innerText);
 
-             data[id].splice([i],1);
+                 var id = +(ui.sender[0].id);
+                 for (var i=0; i<data[id].length; i++) {
+                     if (data[id][i]==ui.item[0].innerText) {break;}
+                 }
 
-             localStorage.setItem("todoData", JSON.stringify(data));
+                 data[id].splice([i],1);
+
+                 // localStorage.setItem("todoData", JSON.stringify(data));
+
+                 $.ajax({
+                     url: '/save',
+                     method: 'POST',
+                     data: JSON.stringify(data),
+                     contentType: 'application/json; charset=utf-8',
+                 })
+
+             }.bind(ui));
 
          }
      });
@@ -57,15 +94,29 @@ $(function () {
     $('input').keydown(function(e) {
 
         if (e.keyCode == 13) {
-            var item = $(this).val();
-            var id = +($(this).parent().parent()[0].id);
-            var data = JSON.parse(localStorage.getItem("todoData"));
-            data = data || [[],[],[],[],[],[],[],[]];
-            data[id].push(item);
-            localStorage.setItem("todoData", JSON.stringify(data));
+            //var item = $(this).val();
+            //var id = +($(this).parent().parent()[0].id);
+            //var data = JSON.parse(localStorage.getItem("todoData"));
+            //data = data || [[],[],[],[],[],[],[],[]];
 
-            $(this).parent().parent().append('<li class="Bullet">' + item + '</li>');
-            $(this).val('');
+            $.post('/load',function (result) {
+                var item = $(this).val();
+                var data = result[0] || [[],[],[],[],[],[],[],[]];
+                var id = +($(this).parent().parent()[0].id);
+                data[id].push(item);
+                //localStorage.setItem("todoData", JSON.stringify(data));
+
+                $.ajax({
+                    url: '/save',
+                    method: 'POST',
+                    data: JSON.stringify(data),
+                    contentType: 'application/json; charset=utf-8',
+                });
+
+                $(this).parent().parent().append('<li class="Bullet">' + item + '</li>');
+                $(this).val('');
+            }.bind(this));
+
         }
     });
 
@@ -74,18 +125,32 @@ $(function () {
         drop:function(event,ui) {
 
             var id = +(ui.draggable.parent()[0].id);
-            var data = JSON.parse(localStorage.getItem("todoData"));
-            data = data || [[],[],[],[],[],[],[],[]];
+            //var data = JSON.parse(localStorage.getItem("todoData"));
+            //data = data || [[],[],[],[],[],[],[],[]];
 
-            for (var i=0; i<data[id].length; i++) {
-                if (data[id][i]==ui.draggable[0].innerText) {break;}
-            }
+            $.post('/load',function (result) {
+                var ui =this;
+                var id = +(ui.item.parent()[0].id);
+                var data = result[0] || [[],[],[],[],[],[],[],[]];
 
-            data[id].splice([i],1);
+                for (var i=0; i<data[id].length; i++) {
+                    if (data[id][i]==ui.draggable[0].innerText) {break;}
+                }
 
-            localStorage.setItem("todoData", JSON.stringify(data));
+                data[id].splice([i],1);
 
-            ui.draggable.remove();
+                //localStorage.setItem("todoData", JSON.stringify(data));
+
+                $.ajax({
+                    url: '/save',
+                    method: 'POST',
+                    data: JSON.stringify(data),
+                    contentType: 'application/json; charset=utf-8',
+                });
+
+                ui.draggable.remove();
+            }.bind(ui));
+
         }
 
     });
--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 var express = require('express');
+var db = require('./db');
+var parser = require('body-parser');
 var server = express();
+
+server.use(parser.urlencoded());
+server.use(parser.json());
 
 server.use(express.static('./clients'));
 
-// server.get('/', function (request, respons) {
-//     respons.send('Hello World!');
-// });
+server.post('/load', function (request, response) {
 
-server.listen(3000, function () {
-    console.log('Server start');
+    let td = db.get().collection('todolist');
+    let data;
+    td.find().toArray(function (err,doc) {
+        data = doc;
+        response.json(data);
+    });
+
+});
+
+server.post('/save', function (request, response) {
+    data = request.body;
+
+    let td = db.get().collection('todolist');
+    td.remove({});
+    td.insert(data);
+
+    response.json({status:'ok'});
+});
+
+db.connect('mongodb://localhost:27017/db_todolist', function(err) {
+    if (err) {
+        console.log('Unable to connect to Mongo.');
+        process.exit(1);
+    } else {
+        server.listen(3000, function() {
+            console.log('Server start');
+        })
+    }
 });
